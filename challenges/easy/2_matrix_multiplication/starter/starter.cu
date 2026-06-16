1dc475e6eba313707591be2a0f2d89b316fe6493
curealb <curealb@gmail.com>
2026-06-16 15:16:23 +0800

    Fix matrix multiplication CUDA starter

--- challenges/easy/2_matrix_multiplication/starter/starter.cu
+++ challenges/easy/2_matrix_multiplication/starter/starter.cu
@@ -4,41 +4,41 @@ namespace {
 
 constexpr int TILE_SIZE = 16;
 
-// __global__ void matrix_multiplication_kernel(const float* __restrict__ A,
-//                                              const float* __restrict__ B,
-//                                              float* __restrict__ C, int M,
-//                                              int N, int K) {
-//     __shared__ float tile_a[TILE_SIZE][TILE_SIZE];
-//     __shared__ float tile_b[TILE_SIZE][TILE_SIZE];
+__global__ void matrix_multiplication_kernel(const float* __restrict__ A,
+                                             const float* __restrict__ B,
+                                             float* __restrict__ C, int M,
+                                             int N, int K) {
+  __shared__ float tile_a[TILE_SIZE][TILE_SIZE];
+  __shared__ float tile_b[TILE_SIZE][TILE_SIZE];
 
-//     const int row = blockIdx.y * TILE_SIZE + threadIdx.y;
-//     const int col = blockIdx.x * TILE_SIZE + threadIdx.x;
+  const int row = blockIdx.y * TILE_SIZE + threadIdx.y;
+  const int col = blockIdx.x * TILE_SIZE + threadIdx.x;
 
-//     float sum = 0.0f;
+  float sum = 0.0f;
 
-//     for (int tile = 0; tile < N; tile += TILE_SIZE) {
-//         const int a_col = tile + threadIdx.x;
-//         const int b_row = tile + threadIdx.y;
+  for (int tile = 0; tile < N; tile += TILE_SIZE) {
+    const int a_col = tile + threadIdx.x;
+    const int b_row = tile + threadIdx.y;
 
-//         tile_a[threadIdx.y][threadIdx.x] =
-//             (row < M && a_col < N) ? A[row * N + a_col] : 0.0f;
-//         tile_b[threadIdx.y][threadIdx.x] =
-//             (b_row < N && col < K) ? B[b_row * K + col] : 0.0f;
+    tile_a[threadIdx.y][threadIdx.x] =
+        (row < M && a_col < N) ? A[row * N + a_col] : 0.0f;
+    tile_b[threadIdx.y][threadIdx.x] =
+        (b_row < N && col < K) ? B[b_row * K + col] : 0.0f;
 
-//         __syncthreads();
+    __syncthreads();
 
-// #pragma unroll
-//         for (int i = 0; i < TILE_SIZE; ++i) {
-//             sum += tile_a[threadIdx.y][i] * tile_b[i][threadIdx.x];
-//         }
+#pragma unroll
+    for (int i = 0; i < TILE_SIZE; ++i) {
+      sum += tile_a[threadIdx.y][i] * tile_b[i][threadIdx.x];
+    }
 
-//         __syncthreads();
-//     }
+    __syncthreads();
+  }
 
-//     if (row < M && col < K) {
-//         C[row * K + col] = sum;
-//     }
-// }
+  if (row < M && col < K) {
+    C[row * K + col] = sum;
+  }
+}
 
 }  // namespace
 
